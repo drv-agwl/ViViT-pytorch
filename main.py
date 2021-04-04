abe2fce2ddb63ea1e0ea076705018ae4b32a31c5
OverGeek <drv.agwl@gmail.com>
2021-04-04 22:04:01 +0530

    main.py added

---
+++ main.py
@@ -0,0 +1,22 @@
+import torch
+from models import ViViTBackbone
+
+v = ViViTBackbone(
+    t=32,
+    h=64,
+    w=64,
+    patch_t=8,
+    patch_h=4,
+    patch_w=4,
+    num_classes=10,
+    dim=512,
+    depth=6,
+    heads=10,
+    mlp_dim=8,
+    model=3
+)
+
+device = torch.device('cpu')
+vid = torch.rand(32, 3, 32, 64, 64).to(device)
+
+pred = v(vid)  # (32, 10)
